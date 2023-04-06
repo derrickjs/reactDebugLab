2943fa433eba0c450115fe422b89f12a2cbd9fa4
derrickjs <stahlman.derrick@gmail.com>
2023-04-06 09:31:43 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"; // component was not capitalized. Found this by looking carfully for syntax errors.
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){ // missing constructor .  Using constructor helps initialize the state of the componant
+  super(props);  // missing super(props) which is needed to access the this keyword from componant.
+
+  //missing this keyword. Must use this here so that handlebutton click can properly use the this keyword.
+  this.state = {   
+    message: "Welcome to my React app",
+    counter: 0
+  };
+
+}
+// function below should have been an arrow function.  Just best practice with react to use ES6.
+  handleButtonClick = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.message}</h1>
+        <button onClick={this.handleButtonClick}>Click me</button>
+        <p>You have clicked the button {this.state.counter} times.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
